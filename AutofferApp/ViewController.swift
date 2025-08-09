98946506aa4fa87a5b48266fe49ce55cf0f5ff22
Roei2606 <Roei2606@gmail.com>
2025-08-09 18:08:31 +0300

    Initial Commit

---
+++ AutofferApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutofferApp
+//
+//  Created by Roei Hakmon on 09/08/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AutofferAppTests/AutofferAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  AutofferAppTests.swift
+//  AutofferAppTests
+//
+//  Created by Roei Hakmon on 09/08/2025.
+//
+
+import Testing
+@testable import AutofferApp
+
+struct AutofferAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
